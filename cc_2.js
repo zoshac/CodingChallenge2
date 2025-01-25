0c9680373d339d90d360c6debc3eac06d868b843
Zosha Chung <czosha@gmail.com>
2025-01-25 18:17:15 -0500

    Task 2- Student Scores

--- cc_2.js
+++ cc_2.js
@@ -12,3 +12,14 @@ console.log(products);
 // Store Inventory
 
 // Task 2- Modifying Arrays 
+
+let scores = [58, 92, 99, 64,80];
+scores[1] = 90;
+
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+
+console.log("Updated Scores:", scores);
+console.log("Average Scores:", average);
+
+// Student Scores 
