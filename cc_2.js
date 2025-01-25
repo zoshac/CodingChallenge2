761bddc3b874b931a43c1646de64c0d7299418d8
Zosha Chung <czosha@gmail.com>
2025-01-25 18:01:00 -0500

    Task 1- Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,14 @@
+// Task 1- Working with Arrays
+
+let products = ["Mascara", "Foundation", "Concealer", "Highlight", "Blush"];
+
+// New product added 
+products.push("Eyeliner");
+
+// Last product removed 
+products.pop();
+
+console.log(products);
+// Store Inventory
+
+// Task 2- Modifying Arrays 
